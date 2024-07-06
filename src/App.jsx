9dd6fe60e9d618ef380bf4a20c235ef8bb852a50
wkauan <kauanmbomfim5@gmail.com>
2024-07-05 21:23:40 -0300

    feat: Create project components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+// CSS Import
 import "./App.css"
+
+// React Imports
+import { useCallback, useState, useEffect } from "react"
+
+// Data Import
+import { wordsList } from "./data/Words"
+
+// Componnets Imports
 import StartScreen from "./components/startScreen/StartScreen"
+import Game from "./components/game/Game"
+import GameOver from "./components/gameOver/GameOver"
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
   return (
     <>
       <div className="App">
-        <StartScreen />
+        {gameStage === "start" && <StartScreen />}
+        {gameStage === "game" && <Game />}
+        {gameStage === "end" && <GameOver />}
       </div>
     </>
   )
